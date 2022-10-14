0687aad49ad908d698beca658065df37eccb7928
herrycyril <herrymomburi.com>
2022-10-14 11:13:03 -0700

    lets see

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,11 +6,16 @@
  */
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
+	int c;
 	for (i = 0; i < 26; i++)
 	{
 		putchar(alp[i]);
+	}
+	while (c = 27; c < 52; c++)
+	{
+		putchar(alp[c]);
 	}
 	putchar('\n');
 	return (0);
